609ea56c1a19c2da8dfc690b4a80cc194f2588c7
TianF0F <137144796+TianF0F@users.noreply.github.com>
2023-06-20 17:38:42 +0800

    Comments added //

--- f2c_convert.cpp
+++ f2c_convert.cpp
@@ -3,11 +3,18 @@ using namespace std;
 
 int main()
 {
+    //Variable initialize
     double farrenheit;
     double celsius;
+
+    //Inputting
     cout << "Please enter Celsius:" << endl;
     cin >> celsius;
+
+    //Formula
     farrenheit = celsius * (9.0/5.0) + 32;
+
+    //Output
     cout << "Farrenheit is " << farrenheit << endl;
     return 0;
 }
